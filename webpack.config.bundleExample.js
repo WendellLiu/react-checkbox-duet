1d299058b52d7325ba7e3658779ba0bfd6c973a2
wendellliu <cuk.bas@gmail.com>
2016-08-25 10:24:20 +0800

    fix demo page bundle file path

--- webpack.config.bundleExample.js
+++ webpack.config.bundleExample.js
@@ -7,7 +7,7 @@ module.exports = {
     './example/index'
   ],
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, 'static'),
     filename: 'bundle.js',
     publicPath: '/static/'
   },
